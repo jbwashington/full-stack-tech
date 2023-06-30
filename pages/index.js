508ff3ef7b6a3bb2bfb4b8c731cf3c9b7a593fdd
James Washington <jbwashington@gmail.com>
2023-06-30 10:33:54 -0400

    updated about text

--- pages/index.js
+++ pages/index.js
@@ -143,7 +143,7 @@ export default function Home() {
     <div className="text-center py-5">
     <h2 className="text-3xl font-semibold mb-4">About Full Stack Tech LLC</h2>
     <p className="px-4 lead">
-    Full Stack Tech is a proud 100% minority-owned business. We value cultural and neurological diversity and inclusivity, not just because it's the right thing to do, but because we believe it makes us stronger and more competetive. Our team is comprised of an amazing mix of talent from all around the world, and we work hard to foster a culture of respect and open-mindedness.
+    Full Stack Tech is a proud 100% minority-owned business. We value gender, cultural and neurological diversity and inclusivity, not just because it's the right thing to do, but because we believe it makes us stronger and more competetive. Our team is comprised of an amazing mix of talent from all around the world, and we work hard to foster a culture of respect and open-mindedness.
     </p>
     <p className="px-4 mt-4 lead">
     We're always on the lookout for people who are constantly innovating and reimagining the status quo. If you're someone who's curious, talented, and fun to work with, we'd love to hear from you.
