17c687f22033a1adb3ba4491046a2bf259f1f1e8
James Washington <jbwashington@gmail.com>
2023-07-14 15:52:10 -0400

    started fixing mailgun template api call

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,31 +1,29 @@
-const mailgun = require('mailgun.js');
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
 
-const DOMAIN = process.env.MAILGUN_DOMAIN;
-
-const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOMAIN
+const mg = mailgun.client({
+  username: 'api',
+  key: process.env.MAILGUN_API_KEY,
+  domain: process.env.VERIFIED_DNS_NAME,
 });
-
-const data = {
-  from: `Full Stack Tech Team <noreply@${process.env.MAILGUN_DOMAIN}>`,
-  to: [email],
-  subject: "Your Requested Free Guide to Data-Driven Business Growth",
-  template: "Guide to Data-Driven Business Growth",
-}
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email } = req.body;
 
+    const data = {
+      from: `Full Stack Tech Team <noreply@${process.env.VERIFIED_DNS_NAME}>`,
+      to: email,
+      subject: "Your Requested Free Guide to Data-Driven Business Growth",
+      template: "Guide to Data-Driven Business Growth",
+      'h:X-Mailgun-Variables': JSON.stringify({test: "test"})
+    }
+
     try {
+      const body = await mg.messages.create(process.env.VERIFIED_DNS_NAME, data);
 
-      const response = await mg.messages().send(data, function (error, body) {
-        console.log(body);
-      }
-      );
-
-      if (response) {
+      if (body) {
         res.status(200).json({ message: "You have been subscribed successfully!" });
       } else {
         res.status(500).json({ message: "An error occurred while subscribing. Please try again." });
